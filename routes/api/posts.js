a4c9df908daf63672d6362ed901185d30cefec70
Abisake <abisake123@gmail.com>
2020-05-23 02:00:44 -0700

    Ninth Commit

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -183,10 +183,10 @@ router.post('/comment/:id', [auth, [
             avatar: user.avatar,
             user: req.user.id
         } // no need to create as an class object,because we are not creating an object for an db,instead it is a part of collection
-        post.comment.unshift(newComment)
+        post.comments.unshift(newComment)
         await post.save()
 
-        res.json(post.comment);
+        res.json(post.comments);
     } catch (err) {
         console.error(err.message)
         res.status(400).json({
@@ -195,7 +195,7 @@ router.post('/comment/:id', [auth, [
     }
 })
 
-//@route   Delete api/posts/comment/:id/comment_id
+//@route   Delete api/posts/comment/:id/:comment_id
 //@desc    delete a comment on a post
 //@access  private
 router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
@@ -204,17 +204,18 @@ router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
         //pull out the comment
         const comment = post.comments.find(comment => comment.id === req.params.comment_id)
         //Check whether comment exists or not
-        if (!comment)
+        if (!comment) {
             return res.status(404).json({
                 msg: "Comment does not exist"
             })
+        }
         //check whether comment is made by this user or not
         if (comment.user.toString() !== req.user.id)
             return res, status(404).json({
                 msg: "User not authorized"
             })
         //removing from the db
-        const removeIndex = post.comments.map(cmt => cmt.user.toString()).indexOf(req.user.id)
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id)
         post.comments.splice(removeIndex, 1)
         await post.save()
         res.json(post.comments)
